d723d4db26cf0e5c140b30e77985be5a5d805fa1
Yulia <yulia.hydrologist@yandex.ru>
2019-12-13 11:53:19 +0300

    change module 6

--- Module_6_EUV.py
+++ Module_6_EUV.py
@@ -10,14 +10,17 @@ import math
 import datetime
 import csv
 import re
+from collections import OrderedDict
 
+print("module 6")
 sut=datetime.timedelta(1)
 
-builtins.daty=[date_n]
-data=date_n
-while data<date_k:
+builtins.daty=[builtins.date_n]
+data=builtins.date_n
+while data<builtins.date_k:
     data=data+sut
-    daty.append(data)
+    builtins.daty.append(data)
+print(builtins.daty)
 
 with open(str(builtins.kat)+"/"+str(builtins.year_now)+"/ЕУВ.txt","r") as inf:
     euv_file=[]
@@ -84,18 +87,84 @@ euv_m.append(m11)
 euv_m.append(m12)
 
 euv_per=[]
+print(builtins.month_n, builtins.day_n)
+print(builtins.month_k, builtins.day_k)
+if builtins.month_n != builtins.month_k:
+    euv_per.append((euv_m[builtins.month_n-1])[(builtins.day_n-1):])
 
-euv_per.append((euv_m[builtins.month_n-1])[(builtins.day_n-1):])
+    for mes in range(builtins.month_n,builtins.month_k-1):
+        euv_per.append(euv_m[mes])
 
-for mes in range(builtins.month_n,builtins.month_k-1):
-    euv_per.append(euv_m[mes])
-
-euv_per.append((euv_m[builtins.month_k-1])[:builtins.day_k])
+    euv_per.append((euv_m[builtins.month_k-1])[:builtins.day_k])
+else:
+    euv_per.append((euv_m[builtins.month_n-1])[(builtins.day_n-1):builtins.day_k])
+print(euv_per)
 
 builtins.H_euv=[]
 for mes in euv_per:
     for i in range(len(mes)):
         builtins.H_euv.append(float(mes[i]))
+
+'''
+Нижеизложенный блок используется, если в периоде отмечено
+неустановившееся движение потока
+'''
+pattern = "неуст. движ. потока"
+match = re.search(pattern, builtins.reason)
+print(len(builtins.H_euv))
+if match:
+    print("неуст. движ. потока")
+    builtins.dict_H = {}
+    for i in range(len(builtins.H_euv)):
+        print(builtins.H_euv[i])
+        builtins.dict_H[builtins.daty[i]] = builtins.H_euv[i]
+    
+    print(builtins.dict_H)
+
+    #Подсчитываем количество локальных максимумов в периоде (пиков паводка/половодья)
+    #builtins.n_max = 0
+    #Даты пиков паводка/половодья
+    builtins.dates_max = []
+    for key in OrderedDict(builtins.dict_H).keys():
+        if (key >= builtins.date_n + datetime.timedelta(2) and key <= builtins.date_k - datetime.timedelta(2) and\
+           builtins.dict_H[key - datetime.timedelta(2)] < builtins.dict_H[key - datetime.timedelta(1)] and\
+           builtins.dict_H[key - datetime.timedelta(1)] < builtins.dict_H[key] and\
+           builtins.dict_H[key] > builtins.dict_H[key + datetime.timedelta(1)] and\
+           builtins.dict_H[key + datetime.timedelta(1)] > builtins.dict_H[key + datetime.timedelta(2)]) or \
+           (key >= builtins.date_n + datetime.timedelta(2) and key <= builtins.date_k - datetime.timedelta(2) and\
+           builtins.dict_H[key - datetime.timedelta(2)] > builtins.dict_H[key - datetime.timedelta(1)] and\
+           builtins.dict_H[key - datetime.timedelta(1)] > builtins.dict_H[key] and\
+           builtins.dict_H[key] < builtins.dict_H[key + datetime.timedelta(1)] and\
+           builtins.dict_H[key + datetime.timedelta(1)] < builtins.dict_H[key + datetime.timedelta(2)]):
+    
+            #builtins.n_max += 1
+            builtins.dates_max.append(key)
+            print(key)
+    '''
+    #Подсчитываем количество локальных минимумов в периоде (переходов с одной водны паводка/половодья на другую)
+    #builtins.n_min = 0
+    #Даты переходов с одной водны паводка/половодья на другую
+    builtins.dates_min = []
+    for key in OrderedDict(builtins.dict_H).keys():
+        print(builtins.dict_H[key])
+        if key >= builtins.date_n + datetime.timedelta(2) and key <= builtins.date_k - datetime.timedelta(2) and\
+           builtins.dict_H[key - datetime.timedelta(2)] > builtins.dict_H[key - datetime.timedelta(1)] and\
+           builtins.dict_H[key - datetime.timedelta(1)] > builtins.dict_H[key] and\
+           builtins.dict_H[key] < builtins.dict_H[key + datetime.timedelta(1)] and\
+           builtins.dict_H[key + datetime.timedelta(1)] < builtins.dict_H[key + datetime.timedelta(2)]:
+    
+            #n_min += 1
+            builtins.dates_min.append(key)
+            print(key)
+            '''
+        
+    #print(n_max)
+    print(builtins.dates_max)
+
+    #print(n_min)
+    #print(builtins.dates_min)
+else:
+    pass
 
 import Module_7_ERV_KR
 
